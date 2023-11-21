62dc93fa76626d465dddf85974c49617be4f9ac7
Annie de Bry <89087287+anniedebry@users.noreply.github.com>
2023-11-21 12:12:30 -0700

    fixed title screen background movement
    movement now depends on the amount of pixels the background has moved rather than the amount of time elapsed

--- Scenes/TitleBackgroundMovement.gd
+++ Scenes/TitleBackgroundMovement.gd
@@ -1,11 +1,17 @@
 extends Sprite2D
 
+# backgourd re-positions itself based off the amount of pixels it has moved, slightly choppy because the sprite isn't completely even
+
 var move_timer : float = 0.0
-var move_interval : float = 5.0
-var move_duration : float = 0.79899
-var move_distance : int = 2
+var pixels_moved : int = 0
+var move_interval : float = 2.0
+#var move_duration : float = 0.79899
+var pixels_to_move : int = 113
+var move_distance : int = 1
 var original_position : Vector2
 var animation_started : bool = false
+
+#28 pixels
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,21 +20,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# Update the timer
-	move_timer += delta
 	
 	if animation_started == false:
 		position.x += move_distance
 		position.y -= move_distance
+		pixels_moved += 1
 	
 	# Check if it's time to move
 	if move_timer >= move_interval:
 		# Move the sprite
 		position.x += move_distance
 		position.y -= move_distance  # Move up (negative Y direction)
+		pixels_moved += 1
 
 	# Check if it's time to repeat the animation
-	if move_timer >= move_duration:
+	if pixels_to_move <= pixels_moved:
 		# Reset the sprite's position after 10 seconds
 		position = original_position
-		move_timer -= move_duration  # Adjust the timer to prevent immediate reset
+		pixels_moved = 0
+		
+		
